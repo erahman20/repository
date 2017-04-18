0b503c4020638ddb60249bb427d0aa93e13ec17c
erahman20 <e.rahman20@gmail.com>
2017-04-18 15:49:05 -0400

    new repository

---
+++ src/UnderStand.java
@@ -0,0 +1,20 @@
+//class A {
+//	int ami_buzte_chai;
+//	public void X() {
+//		System.out.println("Class A");
+//	}
+//	
+//}
+//class B extends A {
+//	public void X() {
+//		System.out.println("Class A");
+//	}
+//}
+
+
+public class UnderStand {
+	public static void main(String[] args) {
+		System.out.println('a'+ 0); 
+	}
+	
+}
---
+++ src/WorldHello.java
@@ -0,0 +1,13 @@
+
+public class WorldHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello");
+System.out.println("hihy0]8");
+System.out.println("w");
+System.out.println("Bobby");	
+	
+	}
+
+}
